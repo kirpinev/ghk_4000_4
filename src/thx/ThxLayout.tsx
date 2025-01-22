01f21562545e81e7bb0e388c6333cce3822204f0
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-01-22 11:41:54 +0300

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -1,22 +1,10 @@
 import { Typography } from "@alfalab/core-components/typography";
-import { ButtonMobile } from "@alfalab/core-components/button/mobile";
 import moai from "../assets/moai.png";
 import { thxSt } from "./style.css";
-import { appSt } from "../style.css.ts";
 import { useEffect } from "react";
 import { LS, LSKeys } from "../ls";
 
 export const ThxLayout = () => {
-  const clickSuccess = () => {
-    if (!LS.getItem(LSKeys.ClickThanks, false)) {
-      window.gtag("event", "4000_thnx_click", {
-        variant_name: "ghk_4000_4",
-      });
-
-      LS.setItem(LSKeys.ClickThanks, true);
-    }
-  };
-
   useEffect(() => {
     if (!LS.getItem(LSKeys.ShowThanks, false)) {
       window.gtag("event", "4000_end_page_view", {
@@ -49,12 +37,6 @@ export const ThxLayout = () => {
           Вы поучаствовали в очень важном исследовании, которое поможет улучшить
           продукт. Вы – наш герой!
         </Typography.Text>
-      </div>
-
-      <div className={appSt.bottomBtnThx}>
-        <ButtonMobile block view="primary" href="" onClick={clickSuccess}>
-          Спасибо, понятно!
-        </ButtonMobile>
       </div>
     </>
   );
